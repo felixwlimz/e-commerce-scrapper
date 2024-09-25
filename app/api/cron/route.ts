c216f6cb648265bc22b44e777dac5956c6173e82
Felix Winston <felixlim140@gmail.com>
2024-09-25 20:43:19 +0700

    Fixed Build Error

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import Product from "@/lib/models/product.model";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 import { scrapAmazonProduct } from "@/lib/scrapper";
 
-export const maxDuration = 300; // This function can run for a maximum of 300 seconds
+export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
